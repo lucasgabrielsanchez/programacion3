f3cbb9ff4c41e2bcd180b63f071bce9738837178
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2017-09-05 22:15:38 -0300

    Lab3Clase3

---
+++ Lab3Clase3/script/miscript.js
@@ -0,0 +1,14 @@
+// alert("Hola Mundo");
+var a = 1; //number
+alert(a);
+alert(typeof(a));
+a=true//boolean
+
+//puntero a función
+a=function(){alert("soy una funcion");}
+
+var miArray = ["Enero",1,"Marzo"];
+typeof(miArray[0]);
+
+//esto evalua si son iguales y son del mismo tipo
+if(miArray[0] === "string");
